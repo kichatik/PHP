fb9aafef6077eb01159848f66cc3741430da7338
Anzhelika Kichatova <146697683+kichatik@users.noreply.github.com>
2024-11-11 12:59:33 +0200

    Update index.php

--- kinobaza_php/index.php
+++ kinobaza_php/index.php
@@ -3,9 +3,6 @@ include_once 'model/Model.php';
 include_once 'controller/controller.php';
 include_once 'route/routing.php';
 
-// Set the response content
-$response = Controller::StartSite();
-
 // Output the response (content)
 echo $response;
 ?>
