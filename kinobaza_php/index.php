674ba92ec314f346ee9c4b3fefa61d6f6c686690
User <alena.kuznetsova@ivkhk.ee>
2024-11-11 12:17:14 +0200

    .

--- kinobaza_php/index.php
+++ kinobaza_php/index.php
@@ -2,7 +2,9 @@
 include_once 'model/Model.php';
 include_once 'controller/controller.php';
 include_once 'route/routing.php';
+
+// Assuming $response is supposed to be set in the controller:
+$response = getResponseFromController(); // Replace with actual function call
+
 echo $response;
-
-
- ?>
+?>
